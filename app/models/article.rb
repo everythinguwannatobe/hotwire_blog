28b34bc4a6223423e761d7385229e3d4b990ddaa
spencerduncanpower <timdonerpower@gmail.com>
2021-11-07 09:50:55 -0500

    Fix user model to default to Current attribute

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, default: -> { Current.user }
 
   has_rich_text :body
 
-  validates :title, presence: true, length: {maximum: 50}
+  validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true
 end
